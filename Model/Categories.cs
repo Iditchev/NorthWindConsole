131e5ebc1dbeb3eada54154b776492134ff8ed47
BIT student <bitstudent@bitnet.wctc.edu>
2021-04-12 13:47:05 -0500

    Data Validation with Annotations

--- Model/Categories.cs
+++ Model/Categories.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -15,6 +17,7 @@ namespace NorthWindConsole.Model
         }
 
         public int CategoryId { get; set; }
+         [Required]
         public string CategoryName { get; set; }
         public string Description { get; set; }
 
