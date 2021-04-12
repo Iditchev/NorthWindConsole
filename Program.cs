806ca1559e3d47e98198664116a2c56ffb888dfc
BIT student <bitstudent@bitnet.wctc.edu>
2021-04-12 13:53:36 -0500

    Option 3- Display products for chosen category

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace NorthwindConsole
                 {
                     Console.WriteLine("1) Display Categories");
                     Console.WriteLine("2) Add Category");
+                    Console.WriteLine("3) Display Category and related products");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
                     Console.Clear();
@@ -54,7 +55,7 @@ namespace NorthwindConsole
                         var isValid = Validator.TryValidateObject(category, context, results, true);
                         if (isValid)
                         {
-                                 var db = new Northwind_DotNetDb_JSGContext();
+                                 var db = new NWConsole_96_IDContext();
                             // check for unique name
                             if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
                             {
@@ -75,6 +76,22 @@ namespace NorthwindConsole
                                 logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
                             }
                         }
+                    }
+                        else if (choice == "3")
+                    {
+                        var db = new NWConsole_96_IDContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryId);
+
+                        Console.WriteLine("Select the category whose products you want to display:");
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        logger.Info($"CategoryId {id} selected");
                     }
                     Console.WriteLine();
 
